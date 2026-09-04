faa7e16aa4aac8617eee17eb9576abcab3200440
mandr <m.andrade.santos2018@gmail.com>
2026-09-04 19:22:52 -0300

    Primeiras tentativas de config

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_meucubo_FILE C:/Users/mandr/Faculdade/Cubo-2x2x2/build/meucubo.exe)
+set(__QT_DEPLOY_TARGET_meucubo_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_meucubo_RUNTIME_DLLS C:/Qt/6.11.2/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.2/mingw_64/bin/Qt6Core.dll)
