b3a6219b4cf284cde4ebe0834130370e8051accb
guimascaretti <guimascaretti@gmail.com>
2026-09-10 14:52:31 -0300

    configuração do cmake

---
+++ build/linux/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_meucubo_FILE /home/guimasca/Documents/Projeto_Cubo/Cubo-2x2x2/build/linux/meucubo)
+set(__QT_DEPLOY_TARGET_meucubo_TYPE EXECUTABLE)
---
+++ build/linux/CMakeFiles/meucubo.dir/meucubo_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/meucubo.dir/meucubo_autogen/mocs_compilation.cpp.o: \
+ /home/guimasca/Documents/Projeto_Cubo/Cubo-2x2x2/build/linux/meucubo_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
