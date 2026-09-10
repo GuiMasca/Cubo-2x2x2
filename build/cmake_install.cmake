8ce59a9edd433b47e1d15bf3537c13f3d84eee66
juliacapereira <carrielj23@outlook.com>
2026-09-10 15:53:43 -0300

    Menu e movimentos funcionando

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 # Install script for directory: C:/Users/julia/OneDrive/Documentos/Cubo-2x2x2
-=======
-# Install script for directory: /home/guimasca/Documents/Projeto_Cubo/Cubo-2x2x2
->>>>>>> origin/Gui
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/MeuCubo")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -31,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -43,18 +34,9 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/Qt/Tools/mingw1310_64/bin/objdump.exe")
 endif()
 
-<<<<<<< HEAD
-=======
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/guimasca/Documents/Projeto_Cubo/Cubo-2x2x2/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
->>>>>>> origin/Gui
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -68,12 +50,8 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-<<<<<<< HEAD
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
   file(WRITE "C:/Users/julia/OneDrive/Documentos/Cubo-2x2x2/build/${CMAKE_INSTALL_MANIFEST}"
-=======
-  file(WRITE "/home/guimasca/Documents/Projeto_Cubo/Cubo-2x2x2/build/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> origin/Gui
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
